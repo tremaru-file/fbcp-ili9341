5d48031f4517f7f68dc9ea77e01078564b2b22a4
drondistortion <cancelbutton@gmail.com>
2019-11-18 13:49:51 +0300

    comments in build

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cmake -DILI9341=ON\
-      -DGPIO_TFT_DATA_CONTROL=12\
-      -DGPIO_TFT_BACKLIGHT=13\
-      -DGPIO_TFT_RESET_PIN=14\
-      -DSPI_BUS_CLOCK_DIVISOR=12\
-      -DSTATISTICS=0 -DBACKLIGHT_CONTROL=ON ..\
+      -DGPIO_TFT_DATA_CONTROL=12 `# Вывод DATA CONTROL (D/C)`\
+#     -DGPIO_TFT_BACKLIGHT=13\ -DBACKLIGHT_CONTROL=ON `# Выключение подсветки по таймеру`\
+      -DGPIO_TFT_RESET_PIN=14 `# Вывод RST`\
+      -DSPI_BUS_CLOCK_DIVISOR=12 `# Делитель для получения CLK SPI (только чётные)`\
+      -DSTATISTICS=0 `# Вывод статистики сверху дисплея (0 - нет, 1 - кратко, 2 - вся статистика)`\
+        ..\
         && make -j
